389c41f53565a3498131124b7c8ad499d3848c22
LahcenNeb <lahcena@nebulaconsulting.co.uk>
2025-02-22 15:13:34 +0000

    first draft

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = this.read;
+}
+
+function addBookToLibrary() {
+    // take params, create a book then store it in the array
+}
+
+// add book function
