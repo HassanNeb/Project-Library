854996070f3fb33e6cdac6135eda885a654cd40b
LahcenNeb <lahcena@nebulaconsulting.co.uk>
2025-02-23 09:49:52 +0000

    js modified

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 const myLibrary = [];
 
 // constructor to create the object book
-function Book(name, author, pages, isRead) {
+function Book(name, author, pages, read) {
     this.name = name;
     this.author = author;
     this.pages = pages;
-    this.isRead = this.isRead;
+    this.read = read;
 }
 // function to add Books to the Library
-function addBookToLibrary(name, author, pages, isRead) {
-    const newBook = new Book((name, author, pages, isRead));
+function addBookToLibrary() {
+    let name = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").checked;
+    const newBook = new Book(name, author, pages, read);
     myLibrary.push(newBook);
+    displayBooks();
 }
 
 //function to display Books on the Page
-
 function displayBooks() {
     const bookContainer = document.querySelector(".main");
-
+    bookContainer.innerHTML = "";
     myLibrary.forEach((element) => {
         const bookCard = document.createElement("div");
         bookCard.classList.add("card");
 
         bookCard.innerHTML = `
-        <h3> Title : ${element.title}</h3>
+        <h3> Title : ${element.name}</h3>
         <p>  Author : ${element.author}</p>
         <p>  Pages : ${element.pages}</p>
         <button class="btn"> ${
-            element.isRead ? "Read" : "Not Read"
+            element.read ? "Read" : "Not Read"
         }</button>        
         `;
 
@@ -35,21 +39,13 @@ function displayBooks() {
     });
 }
 
-let book1 = {
-    title: "Clean Code",
-    author: "Robert Martin",
-    pages: 464,
-    isRead: true,
-};
-
 let book2 = {
-    title: "The Hobbit",
+    name: "The Hobbit",
     author: "J.R.R. Tolkien",
     pages: 295,
-    isRead: false,
+    read: false,
 };
 
-myLibrary.push(book1);
 myLibrary.push(book2);
 console.log(myLibrary);
 displayBooks();
@@ -64,4 +60,10 @@ const closeBtn = document.querySelector(".close");
 closeBtn.addEventListener("click", () => {
     const dialog = document.querySelector(".dialog-box");
     dialog.close();
+});
+
+const btnform = document.querySelector("#form");
+btnform.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addBookToLibrary();
 });
